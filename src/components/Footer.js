74eca63bda98c74bc21b9fb4313c038e5b0e022a
hannalalia <lalia_hanna@yahoo.com>
2021-08-04 16:08:31 +0800

    remove fixed footer -disclaimer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 function Footer() {
     return (
-        <footer className="text-center bg-light p-2 fixed-bottom">
+        <footer className="text-center bg-light p-2">
             <i className="">
                 Disclaimer: The data used in this website was pulled from &nbsp; 
                 <a className="text-decoration-none" href="https://github.com/Simperfy/Covid-19-API-Philippines-DOH" target="_blank">Covid-19-API-Philippines-DOH API</a>
